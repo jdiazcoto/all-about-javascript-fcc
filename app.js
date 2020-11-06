629fd279a332174e08f30c27a8abe2ad1af17fbd
Jorge Diaz <jdiaz@alccomputadoras.com>
2020-11-06 10:55:58 -0600

    Up to decrementor operator

---
+++ app.js
@@ -0,0 +1,143 @@
+//1. COMMENTS
+
+var number = 5; //in line comment
+
+/*This is a 
+A
+S
+M
+U
+C
+H
+A
+S
+Y
+O
+U
+W
+A
+N
+T
+a multiline comment */
+
+//2. DATA TYPES:
+//undefined, null, boolean, string, symbol, number and object
+
+//3. VARIABLES
+var myName = "Jorge"; //Way number 1
+
+myName = 8; //Changing it dynamically
+
+let ourName = "JfdcDevOps"; //Way number 2
+
+const pi = 3.14; //Variable that can never change it and Way number 3
+
+var a; //Declaring Variable
+var b = 2; //Assigning Variables
+a = 7; //Assignment
+b = a; //Assignment
+
+console.log(a); //Prints to browser console
+
+//MORE ABOUT VARIABLES
+
+//Uniniatilized vars
+var a;
+var b;
+var c;
+
+//Initializing them
+
+a = 5;
+b = 10;
+c = "I Am a ";
+
+//4. OPERATIONS WITH VARIABLES
+
+a = a + 1; //Or a+=12;
+b = b + 5; //Or b*=9;
+c = c + " String!"; //Or c+=" String!";
+
+//4. CASE SENSITIVITY ABOUT VARIABLES
+
+//Declarations
+var stUdlYCapVar;
+var properCamelCase;
+var TitleCaseOver;
+
+//Assignments
+STUDLYCAPVAR = 10; //Different var than declared
+PRoperCAmelCase = "A String"; //Different var than declared
+titleCaSEOVER = 9000; //Different var than declared
+
+//Correct Declaration
+var studlyCapVar;
+var propoerCamelCase;
+var titleCaseOver;
+
+//Now use them correctly
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//5. ADDING NUMBERS
+var sum = 10 + 0;
+console.log(sume);
+
+//6. SUBTRACTING NUMBERS
+var difference = 10 - 3;
+
+//7. MULTIPLYING NUMBERS
+var product = 8 * 0;
+
+//8. DIVIDING NUMBERS
+var quotient = 66 / 33;
+
+//9. INCREMENTING NUMBERS
+var myVar = 87;
+
+myVar = myVar + 1;
+
+myVar++; //Quicker way
+
+//10. DECREMENTING NUMBErS
+var anotherVar = 11;
+
+anotherVar = anotherVar - 1;
+
+anotherVar--; //Quicker way
+
+//11. DECIMAL NUMBERS
+
+var ourDecimal = 5.7;
+
+var myDecimal = 0.009;
+
+//12. MULTIPLY DECIMALS
+
+var product = 2.0 * 2.5;
+
+console.log(product);
+
+//13. DIVIDE DECIMALS
+
+var quotient = 4.4 / 2.0;
+
+//14. FINDING A REMAINDER
+
+var remainder;
+
+remainder = 11 % 3; //remainder = 2
+
+//you can use remainder operations to find even and odd numbers
+
+//15. COMPOUND ASSIGNMENT WITH AUGMENTED ADDITION
+a = 3;
+b = 17;
+c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+//15. COMPOUND ASSIGNMENT WITH AUGMENTED SUBTRACTION
