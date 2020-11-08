33e7e844a9ea5915d7f65819626fcaec9ba86048
Jorge Diaz <jdiaz@alccomputadoras.com>
2020-11-08 08:16:36 -0600

    All About Functions part 1

--- app.js
+++ app.js
@@ -299,3 +299,125 @@ var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
 console.log(thirdToLastLetterOfFirstName);
 var lastName = "Lovelace";
 var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+//32. WORD BLANKS
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+
+  result =
+    "My " + " " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+
+  return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+console.log(wordBlanks("bike", "slow", "flew", "slowly"));
+
+//33. STORE MULTIPLE VALUES WITH ARRAYS
+var ourArray = ["John", 23];
+
+var myArray = ["Jorge", 39];
+
+//34. NESTED ARRAY
+var ourArray = [
+  ["the universe", 42],
+  ["everything", 101010],
+];
+
+var myArray = [
+  ["Bulls", 23],
+  ["White Sox", 45],
+];
+
+//35. ACCESS ARRAY DATA WITH INDEXES
+var ourArray = [50, 60, 70];
+var ourData = [0]; // equals 50
+
+//36. MODIFY ARRAY DATA WITH INDEXES
+var ourArray = [18, 64, 99];
+ourArray[1] = 45; // ourArray now equals [18,45,99]
+
+//37. ACCESS MULTI-DIMENSIONAL ARRAYS WITH INDEXES
+var myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+console.log(myArray[3][0][1]);
+
+//38. MANIPULATE ARRAYS WITH push()
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+myArray.push(["dog"], 3);
+
+//39. MANIPULATE ARRAYS WITH pop()
+var ourArray = [1, 2, 3];
+var removedFromOurArray = ourArray.pop();
+//removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+var removedFromMyArray = myArray.pop();
+
+//40. MANIPULATE ARRAYS WITH shift()
+var ourArray = ["Stimpson", "J", "cat"];
+var removedFromOurArray = ourArray.shift();
+//removeFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]] it removes the first element from the array
+var myArray = [
+  ["John", 23],
+  ["dog", 2],
+];
+
+var removedFromOurArray = myArray.shift();
+
+//41. MANIPULATE ARRAYS WITH unshift()
+var ourArray = ["Stimpson", "J", "cat"];
+ourAarray.shift(); //ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy"); //ourArray now equals ["Happy", "J","cat"]
+
+var myArray = [
+  ["John", 23],
+  ["dog", 2],
+];
+myArray.shift();
+myArray.unshift("Paul", 35);
+
+//42. SHOPPING LIST
+var myList = [
+  ["cereal", 3],
+  ["milk", 2],
+  ["bananas", 3],
+  ["juice", 2],
+  ["eggs", 12],
+];
+
+//43. WRITE REUSABLE CODE WITH FUNCTIONS
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+ourReusableFunction();
+ourReusableFunction();
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
+reusableFunction();
+reusableFunction();
+
+//44. PASSING VALUES TO FUNCTIONS WITH ARGUMENTS
